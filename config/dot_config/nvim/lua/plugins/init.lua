142737697a0226005dcf269c564f78721251bd0b
Raul Ferrando <raulferrando@protonmail.com>
2024-10-08 11:14:33 +0200

    xddd

---
+++ config/dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,20 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+}
