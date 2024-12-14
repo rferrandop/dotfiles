3b92b01af91cc4a9f7f65efb1ccf2b0da83934bc
Raul Ferrando <raulferrando@protonmail.com>
2024-12-14 13:08:17 +0100

    xd

--- config/dot_config/nvim/lua/user/commands.lua
+++ config/dot_config/nvim/lua/user/commands.lua
@@ -1,18 +1,27 @@
-local group = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+local group = vim.api.nvim_create_augroup('user_cmds', { clear = true })
 
-vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
+vim.api.nvim_create_user_command('ReloadConfig', 'source ~/.config/nvim/init.lua', {})
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight on yank',
-  group = group,
-  callback = function()
-    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
-  end,
+	desc = 'Highlight on yank',
+	group = group,
+	callback = function()
+		vim.highlight.on_yank({ higroup = 'Visual', timeout = 300 })
+	end,
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = {'help', 'man'},
-  group = group,
-  command = 'nnoremap <buffer> q <cmd>quit<cr>'
+-- Format file on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end,
 })
 
+-- Set sh filetypes for chezmoi sh templates
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.sh.tmpl",
+	callback = function()
+		vim.bo.filetype = "sh"
+	end
+})
