9a63180bfca98cd5a2f8d54c96b1c24d6f6d46aa
Raul Ferrando <raulferrando@protonmail.com>
2024-12-14 11:02:48 +0100

    nvim: telescope should find hidden files (excluding .git)

--- config/dot_config/nvim/lua/plugins/telescope.lua
+++ config/dot_config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,27 @@
-local Plugin = {'nvim-telescope/telescope.nvim'}
+local Plugin = { 'nvim-telescope/telescope.nvim' }
 
 Plugin.branch = '0.1.x'
 
 Plugin.dependencies = {
-  {'nvim-lua/plenary.nvim'},
-  {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+	{ 'nvim-lua/plenary.nvim' },
+	{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 }
 
-Plugin.cmd = {'Telescope'}
+Plugin.cmd = { 'Telescope' }
 
 function Plugin.init()
-  -- See :help telescope.builtin
-  vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
-  vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
-  vim.keymap.set('n', '<C-o>', '<cmd>Telescope find_files<cr>')
-  vim.keymap.set('n', '<C-g>', '<cmd>Telescope live_grep<cr>')
-  vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
-  vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
+	-- See :help telescope.builtin
+	vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
+	vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
+	vim.keymap.set('n', '<C-o>',
+		"<cmd>lua require'telescope.builtin'.find_files({ find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }})<cr>")
+	vim.keymap.set('n', '<C-g>', '<cmd>Telescope live_grep<cr>')
+	vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
+	vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
 end
 
 function Plugin.config()
-  require('telescope').load_extension('fzf')
+	require('telescope').load_extension('fzf')
 end
 
 return Plugin
