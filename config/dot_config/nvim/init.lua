c2b92e5cc544e6a551bba802d2944897ec625f46
Raul Ferrando <rferrando@okode.com>
2024-12-18 14:46:17 +0100

    neovim: lazyvim

--- config/dot_config/nvim/init.lua
+++ config/dot_config/nvim/init.lua
@@ -1,20 +1,2 @@
-local load = function(mod)
-  package.loaded[mod] = nil
-  require(mod)
-end
-
-load('user.settings')
-load('user.commands')
-load('user.keymaps')
-require('user.plugins')
-
-pcall(vim.cmd.colorscheme, 'tokyonight')
-
--- Neovide configuration
-if vim.g.neovide then
-	vim.o.guifont = "MesloLGS NF:h14"
-
-	-- Sets how long the scroll animation takes to complete (in seconds)
-	vim.g.neovide_scroll_animation_length = 0.1
-	vim.g.neovide_cursor_animation_length = 0.1
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
