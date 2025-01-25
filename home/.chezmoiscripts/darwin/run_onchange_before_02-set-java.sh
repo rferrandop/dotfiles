224b08d435bb8b67233db3ddf0a135113bdd7456
Raul Ferrando <rferrando@okode.com>
2025-01-25 12:25:39 +0100

    darwin: setup java versions

---
+++ home/.chezmoiscripts/darwin/run_onchange_before_02-set-java.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Symlink to system jdks
+sudo ln -sfn /usr/local/opt/openjdk@8/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-8.jdk
+sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+sudo ln -sfn /usr/local/opt/openjdk@21/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-21.jdk
+
+jenv add /Library/Java/JavaVirtualMachines/openjdk-8.jdk/Contents/Home
+jenv add /Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home
+jenv add /Library/Java/JavaVirtualMachines/openjdk-21.jdk/Contents/Home
+
+jenv global 21
