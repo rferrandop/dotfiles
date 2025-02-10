5893fd778de7f4a8845e15b50277063c61f4c508
Raul Ferrando <rferrando@okode.com>
2025-02-10 15:02:00 +0100

    xd

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-dock.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-dock.sh
@@ -23,13 +23,20 @@ declare -a remove_labels=(
 	Numbers
 	Pages
 	"App Store"
+
+    # In order to have a declarative-like dock setup, remove and then add again
+    Alacritty
+    kitty
+    "Brave Browser"
+    "Microsoft Teams"
+    Spotify
 )
 
 for label in "${remove_labels[@]}"; do
 	dockutil --no-restart --remove "${label}" || true
 done
 
-dockutil --add /Applications/Alacritty.app
+dockutil --add /Applications/kitty.app
 dockutil --add "/Applications/Brave Browser.app"
 dockutil --add "/Applications/Microsoft Teams.app"
 dockutil --add /Applications/Spotify.app
