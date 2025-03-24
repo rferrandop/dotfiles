99b21f83183e374a934162140063357aa340a6f8
Raul Ferrando <rferrando@okode.com>
2025-03-24 10:12:37 +0100

    darwin: change bar height

--- home/dot_config/sketchybar/bar.lua
+++ home/dot_config/sketchybar/bar.lua
@@ -3,7 +3,7 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
     topmost = "window",
-    height = 50,
+    height = 40,
     color = colors.bar.bg,
     padding_right = 2,
     padding_left = 2,
